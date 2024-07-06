2c0061253b500aaa81e37a246c3abb6ba1f6ac79
Leonardo Ronnau <leonardodanielronnau76@gmail.com>
2024-07-05 22:40:08 -0300

    add new bG color

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-//var hora = new Date().getHours();
-var hora = 13
+function carregar(){
+
+var hora = new Date().getHours();
+
 var horario = document.getElementById('horario');
 horario.innerHTML = `Agora são ${hora} horas!`
 
@@ -7,8 +9,13 @@ var image = document.querySelector('#image');
 
 if (hora >= 7 && hora <=12) {
 image.src="image/dia.jpg"
+document.body.style.background = '#ff0f00';
 }else if (hora >= 13 && hora <=18) {
 image.src="image/tarde.jpg"
+document.body.style.background = '#00f000'
 }else {
    image.src="image/noite.jpg"
+  document.body.style.background = '#FF00FF'
+}
+
 }
