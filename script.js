2b83265c51ea7f294df957b9e3685780c532e23e
Leonardo Ronnau <leonardodanielronnau76@gmail.com>
2024-07-10 00:01:44 -0300

    add calculation and checkbox years

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-function carregar(){
+function carregar() {
+var anonasc = document.getElementById('txtnascimento').value
+  var anoAtual = new Date().getFullYear()
+  var ano = anoAtual - anonasc
+  var result = document.querySelector('.res')
+ var sexo = document.getElementsByName('sexo')
 
-var hora = new Date().getHours();
-
-var horario = document.getElementById('horario');
-horario.innerHTML = `Agora são ${hora} horas!`
-
-var image = document.querySelector('#image');
-
-if (hora >= 7 && hora <=12) {
-image.src="image/dia.jpg"
-document.body.style.background = '#ff0f00';
-}else if (hora >= 13 && hora <=18) {
-image.src="image/tarde.jpg"
-document.body.style.background = '#00f000'
-}else {
-   image.src="image/noite.jpg"
-  document.body.style.background = '#FF00FF'
+if (sexo[0].checked ){
+  result.innerHTML = `Você é um Homem de ${ano} ANOS`
+  
+}else if (sexo[1].checked){
+    result.innerHTML = `Você é um Mulher de ${ano} ANOS`
+  
+}else if(sexo[2].checked) {
+  result.innerHTML = `Você é uma Crinaça de ${ano} ANOS`
 }
-
 }
