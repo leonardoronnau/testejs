1cc6a6f3ca732f7de4cbb283c91fa9acde7add1c
Leonardo Ronnau <leonardodanielronnau76@gmail.com>
2024-07-10 00:19:35 -0300

    finish testing

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ var anonasc = document.getElementById('txtnascimento').value
   var ano = anoAtual - anonasc
   var result = document.querySelector('.res')
  var sexo = document.getElementsByName('sexo')
-
+var img = document.createElement('img')
 if (sexo[0].checked ){
   result.innerHTML = `Você é um Homem de ${ano} ANOS`
-  
+  img.src = 'image/dia.jpg'
+   result.appendChild(img) 
+   
 }else if (sexo[1].checked){
     result.innerHTML = `Você é um Mulher de ${ano} ANOS`
-  
+    img.src = 'image/tarde.jpg'
+   result.appendChild(img) 
 }else if(sexo[2].checked) {
   result.innerHTML = `Você é uma Crinaça de ${ano} ANOS`
+  img.src = 'image/noite.jpg'
+  result.appendChild(img) 
 }
 }
